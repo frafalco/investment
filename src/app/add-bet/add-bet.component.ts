b7090261184c6bb7747eac4df453a16687ea6704
Kevin mask <jasper.cirque5738@eagereverest.com>
2024-10-02 22:59:28 +0200

    bambobrutto

--- src/app/add-bet/add-bet.component.ts
+++ src/app/add-bet/add-bet.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,6 +10,43 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
   templateUrl: './add-bet.component.html',
   styleUrl: './add-bet.component.css'
 })
+
 export class AddBetComponent {
 
+  public Bet = Bet;
+  console = console
+  pageLoading = true;
+  loading = false;
+  isInitialized = false;
+  setupForm!: FormGroup;
+  errorMessage: string | null = null;
+
+  options = ['Won', 'Lost', 'Pending'];
+
+  bambobrutto: any[] = [
+    { id: 1, value : 'Pending' }, 
+    { id: 2, value : 'Lost' },
+    { id: 3, value : 'Won' }
+  ];
+
+
+  
+  async onSubmitSetup(): Promise<void> {
+
+    this.loading = true;
+
+      
+    
+    this.loading = false;
+  }
+  
+
+}
+
+
+
+export enum Bet {
+  Pending = "Pending",
+  Won = "Won",
+  Lost = "Lost"
 }
