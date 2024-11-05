8a8057251d8d55aeab879e5e04627adf92fcb194
Francesco Falconaro <francesco.falconaro@nttdata.com>
2024-11-05 15:16:37 +0100

    work on signal date

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,6 +6,7 @@ import { selectProfile } from '../store/profile.selector';
 import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { DashboardGraphComponent } from '../dashboard-graph/dashboard-graph.component';
+import { Bet } from '../models/bet.model';
 
 @Component({
   selector: 'app-home',
@@ -16,8 +17,16 @@ import { DashboardGraphComponent } from '../dashboard-graph/dashboard-graph.comp
 })
 export class HomeComponent {
   profile$: Observable<Profile | undefined>;
+  initialBet: Bet = {bookmaker: '', unit: 0, bet: 0, result: '', strategy_id: 0, event: ''};
   
   constructor(private store: Store<AppState>) {
     this.profile$ = store.select(selectProfile);
+  }
+
+  retrieveLastSignalDate(latest: Bet, bet: Bet, index: number, bets: Bet[]): Bet {
+    if(latest?.date) {
+      return new Date(bet.date!) > new Date(latest.date!) ? bet : latest;
+    }
+    return bet;
   }
 }
