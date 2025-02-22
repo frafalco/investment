c7fe3fdb263e0434cd837047162e9e22f2ae43e0
Francesco Falconaro <francesco.falconaro@nttdata.com>
2025-02-22 15:53:37 +0100

    fixing mobile

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NgbDropdownModule, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbCollapseModule, NgbDropdownModule, NgbNavModule, NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap';
 import { NavigationEnd, Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Profile } from '../models/profile.model';
@@ -12,15 +12,16 @@ import * as ProfileActions from '../store/profile.actions';
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [NgbDropdownModule, RouterLink, CommonModule, NgbNavModule],
+  imports: [NgbDropdownModule, RouterLink, CommonModule, NgbNavModule, NgbCollapseModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
   activeItem: string = '';
   username$: Observable<string | undefined>;
+  isMenuCollapsed: boolean = true;
 
-  constructor(private store: Store<AppState>, private router: Router) {
+  constructor(private store: Store<AppState>, private router: Router, private offcanvasService: NgbOffcanvas) {
     this.username$ = store.select(selectUsername);
   }
 
@@ -37,5 +38,9 @@ export class SidebarComponent {
     // await this.supabase.signOut();
     this.store.dispatch(ProfileActions.logout());
     this.router.navigate(['/']);
+  }
+
+  openSidebar(content: any) {
+    this.offcanvasService.open(content, { position: 'start', panelClass: 'bg-dark text-white' });
   }
 }
