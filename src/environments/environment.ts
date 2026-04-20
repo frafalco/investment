19fefb71be7c7eebb0157f8bb076d41a6ac7356d
Francesco Falconaro <francesco.falconaro@nttdata.com>
2026-04-20 12:47:58 +0200

    change db

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    supabaseUrl: 'https://ghnwveiaqnqjmslmybwo.supabase.co',
-    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdobnd2ZWlhcW5xam1zbG15YndvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc3MDY1NzAsImV4cCI6MjA0MzI4MjU3MH0.NnuxDz5YsAUVQsMgGdYfnFCAr-wXZJxRFMeFZ2nOw6g',
+    supabaseUrl: 'https://blnknffpsivtatbozcmb.supabase.co',
+    supabaseKey: 'sb_publishable_6389n3TpEAv_wQ3JYElbKg_XhHSHP_V',
   }
